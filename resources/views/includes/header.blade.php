182487444a236f5421fd6f5c3a6665b6dbfd0195
Valentina-Fr <v_frasca@virgilio.it>
2021-10-01 16:20:13 +0200

    Add header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="logo" id="logo">
+        </div>
+        <div class="col-8">
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </div>
+    </div>
+  </div>
